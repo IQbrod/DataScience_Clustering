a30d3f60db0330bc8a486dd9684c35418a63b296
IQbrod <iqbrod@outlook.fr>
2018-12-05 13:57:15 +0100

    Clustering et coupe de dendogramme

--- main.R
+++ main.R
@@ -24,3 +24,23 @@ plot(mens[1:2],col=mens.class$cluster)
 points(mens.class$centers, col = 1:2, pch = 8)
 text(mens[1:2],attributes(mens)$row.names,col=mens.class$cluster,pos=1)
 # Plot de chest/waist (1,2)
+
+## 3 Classification Hierarchique
+D <- dist(mens)
+res <- hclust(D,method="ward")
+plot(res,hang = -1)
+
+res2 <- cutree(res, k = 5)
+res2
+# Assigne un cluster a chaque élément
+
+cent <- NULL
+for(k in 1:5) {cent<-rbind(cent,colMeans(mens[res2 == k,, drop=FALSE]))}
+cent
+
+res3<-hclust(dist(cent)^2,method="cen",members=table(res2))
+
+res4 <- par(mfrow = c(1,2))
+plot(res,labels=FALSE,hang=-1,main="Original Tree")
+plot(res3,labels=FALSE,hang=-1,main="Restart from 5 clusters")
+par(res4)
